36ae71d03a5524f1e7e00b18eb43a92d5ac03466
Rania0805 <raniaaweni.anjorin@mnsu.edu>
2024-02-13 01:17:26 -0600

    Update CurricularModule.js
    just added new buttons

--- src/components/CurricularModule/CurricularModule.js
+++ src/components/CurricularModule/CurricularModule.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Background from "../Background";
-import { blue, white, red } from "../../utils/colors";
+import { blue, white, red, green } from "../../utils/colors";
 import Button from "../Button";
+import RectButton from "../RectButton";
+import { writeToDatabaseConjecture, writeToDatabaseDraft, getConjectureDataByUUID,getConjectureDataByAuthorID, getConjectureDataByPIN, getPoseDataByConjUUID } from "../../firebase/database";
 import { CurricularContentEditor } from "../CurricularModule/CurricularModuleBoxes";
 import { useMachine } from "@xstate/react";
 import { CurricularContentEditorMachine } from "../../machines/curricularEditorMachine";
+
+
 
 const CurricularModule = (props) => {
   const { height, width, conjectureCallback, mainCallback } = props;
@@ -25,17 +29,56 @@ const CurricularModule = (props) => {
         fontWeight={800}
         callback={mainCallback} // Exit Back To Home
       />
+
+      <Button
+        height={height * 0.12}
+        width={width * 0.0950}
+        x={width * 0.15}
+        y={height * 0.52}
+        color={blue}
+        fontSize={21}
+        fontColor={white}
+        text={"Select Conjectures"}
+        fontWeight={800}
+        callback={null}
+      />
       <Button
         height={height * 0.2}
         width={width * 0.1}
         x={width *0.90}
-        y={height *0.9}
+        y={height *0.52}
         color={blue}
         fontSize={24}
         fontColor={white}
-        text={"Conjecture Editor"}
+        text={"New Conjecture"}
         fontWeight={800}
         callback={conjectureCallback}
+      />
+
+      <RectButton
+        height={height * 0.13}
+        width={width * 0.26}
+        x={width * 0.58}
+        y={height * 0.93}
+        color={green}
+        fontSize={width * 0.014}
+        fontColor={white}
+        text={"SAVE DRAFT"}
+        fontWeight={800}
+        callback={ () => writeToDatabaseDraft() } // Implement Save feature
+      />
+
+      <RectButton
+        height={height * 0.13}
+        width={width * 0.26}
+        x={width * 0.45}
+        y={height * 0.93}
+        color={blue}
+        fontSize={width * 0.015}
+        fontColor={white}
+        text={"PUBLISH"}
+        fontWeight={800}
+        callback={() => writeToDatabaseConjecture()} // publish to database
       />
       <CurricularContentEditor height={height} width={width} />
     </>
